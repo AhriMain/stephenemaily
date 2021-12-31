ecc631e0a1fbf035afe5b68da6817df9c81df3a4
Ahri <chethanawe123@gmail.com>
2021-12-31 15:12:32 +0530

    changed

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>console.log(`connected to port: ${PORT}`))
 // Route Handlers
 app.get('/',(req,res)=>{
-    res.json({message:'hello'})
+    res.json({message:'karthicks a bitchAss'})
 })
